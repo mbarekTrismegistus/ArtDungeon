116d0d7ded4452c2adcb26c0ba0b17726f6d81d1
HermesThemes <momoboogeyman2000@gmail.com>
2024-07-13 17:28:53 +0000

    suspense error

--- app/components/post.js
+++ app/components/post.js
@@ -1,19 +1,16 @@
 import prisma from "@/prisma/client";
+import axios from "axios";
 
 
-export default async function Art() {
+export default async function Art(props) {
     
     
-    let data = await prisma.art.findUnique({
-        where: {
-            id: 54
-        }
-    });
+    let data = (await axios.get(`https://dummyjson.com/users/${props.id}`)).data
     
     return (
 
         <div className="flex flex-wrap gap-7">
-            {data.description}
+            {data.firstName}
         </div>
     )
 }
--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react'
 import Art from '@/app/components/post'
 
-export default function page() {
+export default function page({params}) {
 
 
   return (
     <div className='mt-[80px]'>
         <Suspense fallback="loading.b..">
-            <Art/>
+            <Art id={params.id}/>
         </Suspense>
     </div>
   )
